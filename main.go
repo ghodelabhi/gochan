4c605f94550b5e6c06a03d0d9b03278a2e295405
ghodelabhi <developerghodela022@gmail.com>
2020-11-26 13:47:23 +0530

    Update new code for git

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	go callGo()
+	time.Sleep(1 * time.Second)
+
+	// using chanels
+	chanVal := make(chan bool)
+
+	go callGoChan(chanVal)
+	<-chanVal
+
+	fmt.Println("Executed main...")
+
+}
+
+// callGo :
+func callGo() {
+	fmt.Println("Calling callGo...")
+}
+
+// callGoChan :
+func callGoChan(chanVal chan bool) {
+	fmt.Println("Calling callGoChan...")
+
+	chanVal <- true
+}
